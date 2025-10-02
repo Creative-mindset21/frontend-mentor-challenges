964fa3a3c204b84b73a8de0141de06d791a3312c
Creative-mindset21 <tobiawotoye03@gmail.com>
2025-10-03 00:46:49 +0100

    added js validation to the email

---
+++ Newsletter sign-up form with success message/js/script.js
@@ -0,0 +1,29 @@
+const formEl = document.getElementById("form");
+const emailEl = document.getElementById("email");
+const subscribeEl = document.getElementById("subscribe-el");
+const subscribedEl = document.getElementById("subscribed-el");
+const dismissBtn = document.getElementById("dismiss-btn");
+const errorMsg = document.getElementById("error-msg");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+
+  if (data.email.includes("@")) {
+    emailEl.value = "";
+    subscribeEl.classList.add("subscribe");
+    subscribedEl.style.display = "grid";
+  } else {
+    errorMsg.style.display = "inline-block";
+    emailEl.classList.add("error-input");
+  }
+});
+
+dismissBtn.addEventListener("click", () => {
+  subscribeEl.classList.remove("subscribe");
+  subscribedEl.style.display = "none";
+  errorMsg.style.display = "none";
+  emailEl.classList.remove("error-input");
+});
