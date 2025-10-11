456bc93b195b89f9d8dd43603451aaa7145281c1
Creative-mindset21 <tobiawotoye03@gmail.com>
2025-10-12 00:56:35 +0100

    created a function to check the password strength and render the password strength

--- Password generator app/js/script.js
+++ Password generator app/js/script.js
@@ -8,21 +8,14 @@ const formEl = document.getElementById("form");
 const passwordEl = document.getElementById("password");
 const lengthEl = document.getElementById("length");
 
+lengthEl.textContent = slider.value;
+/* CHANGE THE CHARACTER LENGTH WHEN CHANGED AND SLIDER RANGE*/
 slider.addEventListener("input", (e) => {
   const value =
     ((e.target.value - e.target.min) / (e.target.max - e.target.min)) * 100;
   e.target.style.setProperty("--value", `${value}%`);
-});
-
-/* CHANGE THE CHARACTER LENGTH WHEN CHANGED */
-function changeLength() {
   lengthEl.textContent = slider.value;
-  slider.addEventListener("input", () => {
-    lengthEl.textContent = slider.value;
-  });
-}
-
-changeLength();
+});
 
 /* GENERATE PASSWORDS */
 formEl.addEventListener("submit", (e) => {
@@ -51,4 +44,51 @@ formEl.addEventListener("submit", (e) => {
   }
 
   passwordEl.value = pass;
+
+  checkPassword();
 });
+
+/* PASSWORD STRENGTH */
+function checkPassword() {
+  let strength = 0;
+
+  if (passwordEl.value.length >= 12) {
+    strength += 1;
+  }
+
+  /* UPPERCASE CHECK */
+  if (passwordEl.value.match(/[A-Z]/)) {
+    strength += 1;
+  }
+
+  /* LOWERCASE CHECK */
+  if (passwordEl.value.match(/[a-z]/)) {
+    strength += 1;
+  }
+
+  /* NUMBERS CHECK */
+  if (passwordEl.value.match(/[0-9]/)) {
+    strength += 1;
+  }
+
+  /* NUMBERS CHECK */
+  if (passwordEl.value.match(/[^A-Za-z0-9]/)) {
+    strength += 1;
+  }
+
+  console.log(strength);
+
+  renderColor(strength);
+}
+
+/* RENDER PASSWORD STRENGTH */
+function renderColor(value) {
+  const renderBoxes = document.querySelectorAll("#render-box");
+
+  renderBoxes.forEach((box) => box.classList.remove("box-active"));
+
+  if (value >= 2) renderBoxes[0].classList.add("box-active");
+  if (value >= 3) renderBoxes[1].classList.add("box-active");
+  if (value >= 4) renderBoxes[2].classList.add("box-active");
+  if (value >= 5) renderBoxes[3].classList.add("box-active");
+}
