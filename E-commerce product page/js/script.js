ef4daa50dd6a6940f9d4ceae45e1da85af2be01e
Creative Mindset <tobiawotoye03@gmail.com>
2025-10-25 15:51:48 +0100

    updated the cart when the add to cart button is clicked

--- E-commerce product page/js/script.js
+++ E-commerce product page/js/script.js
@@ -80,13 +80,58 @@ const numberEl = document.getElementById("number");
 const plusBtn = document.getElementById("plus-btn");
 const minusBtn = document.getElementById("minus-btn");
 
-let score = 0;
+let score = 1;
 
 plusBtn.addEventListener("click", () => {
   score++;
   numberEl.textContent = score;
 });
 minusBtn.addEventListener("click", () => {
-  if (score > 0) score--;
+  if (score > 1) score--;
   numberEl.textContent = score;
+});
+
+/* ADD THE PRODUCT WHEN THE SUBMIT BUTTON IS CLICKED */
+const addToCart = document.getElementById("add-to-cart");
+const productName = document.querySelector("h1");
+const amount = document.getElementById("current-price");
+const cartContainer = document.getElementById("cart-container");
+
+let htmlEl = "";
+
+addToCart.addEventListener("click", () => {
+  htmlEl = `
+   <div class="flex">
+      <div class="item">
+        <img
+          src="./images/image-product-1-thumbnail.jpg"
+          alt="Shoe Image"
+          class="cart-img"
+        />
+
+        <div class="item-name-amount flex">
+          <span class="item-name">${productName.textContent}</span>
+
+          <div class="amounts">
+            <span class="item-amount"> ${amount.textContent} x ${score} </span>
+            <span class="total-amount">$${
+              Number(amount.textContent.split("$")[1]) * score
+            }</span>
+          </div>
+        </div>
+
+        <button
+          class="delete-btn"
+          id="delete-btn"
+          aria-label="Click to delete the item"
+        >
+          <img src="./images/icon-delete.svg" alt="" />
+        </button>
+      </div>
+
+      <button class="add-to-cart">Checkout</button>
+    </div> 
+  `;
+
+  cartContainer.innerHTML = htmlEl;
 });
